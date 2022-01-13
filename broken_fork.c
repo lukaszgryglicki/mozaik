38fe053da42328c3620168cf388462741da3ce63
Łukasz Gryglicki <lgryglicki@cncf.io>
2022-01-13 16:15:31 +0000

    Indenting
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- broken_fork.c
+++ broken_fork.c
@@ -1,56 +1,56 @@
 
 void process_filelist_fork(char** fnames, int num)
 {
- int i, p1, p2, p3, i1, i2, i3, pp, p;
- g_file_info = (fileinfo*)malloc(sizeof(fileinfo) * num);
- if (!g_file_info) panic(HERE, "malloc fileinfo");
- p1 = fork();
- p2 = fork();
- p3 = fork();
- i1 = !!p1;
- i2 = !!p2;
- i3 = !!p3;
- pp = (i1<<2) + (i2<<1) + i3;
- p = (pp == 7);
- printf("fork: (%d,%d,%d) -> (%d,%d,%d) -> (%d,%d)\n", p1, p2, p3, i1, i2, i3, pp, p);
- for (i=0;i<num;i++) 
-	{
-	  if (p && (i%8) != pp)
-	  {
-	      // printf("Parent %d skips: %d\n", pp, i);
-	      continue;
-	  }
-	  else if (!p && (i%8) != pp)
-	  {
-	      // printf("Child %d skips: %d\n", pp, i);
-	      continue;
-	  }
+    int i, p1, p2, p3, i1, i2, i3, pp, p;
+    g_file_info = (fileinfo*)malloc(sizeof(fileinfo) * num);
+    if (!g_file_info) panic(HERE, "malloc fileinfo");
+    p1 = fork();
+    p2 = fork();
+    p3 = fork();
+    i1 = !!p1;
+    i2 = !!p2;
+    i3 = !!p3;
+    pp = (i1<<2) + (i2<<1) + i3;
+    p = (pp == 7);
+    printf("fork: (%d,%d,%d) -> (%d,%d,%d) -> (%d,%d)\n", p1, p2, p3, i1, i2, i3, pp, p);
+    for (i=0;i<num;i++) 
+    {
+        if (p && (i%8) != pp)
+        {
+            // printf("Parent %d skips: %d\n", pp, i);
+            continue;
+        }
+        else if (!p && (i%8) != pp)
+        {
+            // printf("Child %d skips: %d\n", pp, i);
+            continue;
+        }
 
-	  init_fileinfo(&g_file_info[i]);
-	  g_file_info[i].fname = (char*)malloc((sizeof(char) * strlen(fnames[i])) + 1);
-	  strcpy(g_file_info[i].fname, fnames[i]);
-	  g_file_info[i].i = i;
-	  free((void*)fnames[i]);
-	  fnames[i] = NULL;
-	  if (!process_file(&g_file_info[i], num))
-		{
-			printf("Failed processing: %s\n", g_file_info[i].fname);
-		}
-	}
- printf("Zapis konfiguracji bazy w pliku %s\n", g_dbout_file);
- if (p) 
- {	/* parent wait for child to finish */
-     printf("Parent waits for children\n");
-     for (i=0;i<8;i++) {
-       printf("Parent waits for child %d\n", i+1);
-       wait(NULL);
-     }
-     printf("Parent waited OK\n");
- }
- else
- {   /* children finishes */  
-     printf("Child finishes\n");
-     exit(0);
- }
- save_dbfile(num);
+        init_fileinfo(&g_file_info[i]);
+        g_file_info[i].fname = (char*)malloc((sizeof(char) * strlen(fnames[i])) + 1);
+        strcpy(g_file_info[i].fname, fnames[i]);
+        g_file_info[i].i = i;
+        free((void*)fnames[i]);
+        fnames[i] = NULL;
+        if (!process_file(&g_file_info[i], num))
+        {
+            printf("Failed processing: %s\n", g_file_info[i].fname);
+        }
+    }
+    printf("Zapis konfiguracji bazy w pliku %s\n", g_dbout_file);
+    if (p) 
+    {	/* parent wait for child to finish */
+        printf("Parent waits for children\n");
+        for (i=0;i<8;i++) {
+            printf("Parent waits for child %d\n", i+1);
+            wait(NULL);
+        }
+        printf("Parent waited OK\n");
+    }
+    else
+    {   /* children finishes */  
+        printf("Child finishes\n");
+        exit(0);
+    }
+    save_dbfile(num);
 }
--- conv.c
+++ conv.c
@@ -4,35 +4,35 @@
 
 void conv(char* f1, char* f2)
 {
- FILE *in, *out;
- int i, itemp, ival[10];
- char temp[256];
+    FILE *in, *out;
+    int i, itemp, ival[10];
+    char temp[256];
 
- in = fopen(f1, "r");
- if (!in) return;
- out = fopen(f2, "w");
- if (!out) { fclose(in); return; }
+    in = fopen(f1, "r");
+    if (!in) return;
+    out = fopen(f2, "w");
+    if (!out) { fclose(in); return; }
 
- printf("opened.\n");
- fscanf(in,   "SNAPDIR=%s\n", temp);
- fprintf(out, "SNAPDIR=%s\n", temp);
+    printf("opened.\n");
+    fscanf(in,   "SNAPDIR=%s\n", temp);
+    fprintf(out, "SNAPDIR=%s\n", temp);
 
- fscanf(in,   "PROCESSED=%d\n", &itemp);
- fprintf(out, "PROCESSED=%d\n", itemp);
+    fscanf(in,   "PROCESSED=%d\n", &itemp);
+    fprintf(out, "PROCESSED=%d\n", itemp);
 
- fscanf(in,   "SNAP=%d,%d\n", &ival[0], &ival[1]);
- fprintf(out, "SNAP=%d,%d\n", ival[0], ival[1]);
- while (fscanf(in, "SIZE=%d,%d SNAP=%d,%d,%d COLOR=%d,%d,%d\n", &ival[0], &ival[1], &ival[2], &ival[3], &ival[4], &ival[5], &ival[6], &ival[7]) == 8)
- {
-	fprintf(out, "SIZE=%d,%d SNAP=%d,%d,%d COLOR=%d,%d,%d\n", 
-	 ival[0], ival[1], ival[2], ival[3], ival[4], ival[7], ival[6], ival[5]);
- }
- fclose(in);
- fclose(out);
+    fscanf(in,   "SNAP=%d,%d\n", &ival[0], &ival[1]);
+    fprintf(out, "SNAP=%d,%d\n", ival[0], ival[1]);
+    while (fscanf(in, "SIZE=%d,%d SNAP=%d,%d,%d COLOR=%d,%d,%d\n", &ival[0], &ival[1], &ival[2], &ival[3], &ival[4], &ival[5], &ival[6], &ival[7]) == 8)
+    {
+        fprintf(out, "SIZE=%d,%d SNAP=%d,%d,%d COLOR=%d,%d,%d\n", 
+                ival[0], ival[1], ival[2], ival[3], ival[4], ival[7], ival[6], ival[5]);
+    }
+    fclose(in);
+    fclose(out);
 }
 
 int main(int lb, char** par)
 {
- conv(par[1], par[2]);
- return 0;
+    conv(par[1], par[2]);
+    return 0;
 }
