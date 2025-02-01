df7ce5dd4a0bb6f1108cb40a34bd0fa0b6e57496
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-02-01 14:12:46 +0100

    Update 2025 - wip 4
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- new-2025/best.sh
+++ new-2025/best.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf best.lst best.dat* best_snap
 mkdir best_snap
-find /data/jpg/best -type f -iname '*.jp*g' > best.lst
-./mozaik_mt --generate-db best.lst --db-outfile best.dat --mode-lf --snap-dir best_snap --jqual 66 --snap-x 512 --snap-y 512 --skip-bmpin --skip-bmpout
+find /data/jpg -type f -iname '*.jp*g' > best.lst
+./mozaik_mt --generate-db best.lst --db-outfile best.dat --mode-lf --snap-dir best_snap --jqual 60 --snap-x 512 --snap-y 512 --skip-bmpin --skip-bmpout
 echo -n 'press enter...'
 ./merge_mts best
 rm best.dat.?
